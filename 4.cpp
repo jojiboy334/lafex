0803f347f16585bd40f105064b233b5dad8699e6
Nikita Kurushin <nikkurushin@gmail.com>
2019-04-29 16:45:35 +0300

    4

---
+++ 4.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	cout << "My heart's in the Highlands, my heart is not here\n"
+		<< "My heart's in the Highlands, a-chasing the deer\n"
+		<< "A - chasing the wild deer, and following the roe\n"
+		<< "My heart's in the Highlands wherever I go.\n"
+		<< "Farewell to the Highlands, farewell to the North\n"
+		<< "The birthpace of valour, the country of worth\n"
+		<< "Wherever I wander, wherever I rove\n"
+		<< "The hills of the Highlands for ever I love.\n"
+		<< "Farewell to the mountains high cover'd with snow\n"
+		<< "Farewell to the straths and green valleys below\n"
+		<< "Farewell to the forests and wild - hanging woods\n"
+		<< "Farewell to the torrents and loud - pouring floods.\n"
+		<< '\n';
+	system("pause");
+}
