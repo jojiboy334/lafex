246275cf09985967fefe6d643e8e06ae5645adc5
Nikita Kurushin <nikkurushin@gmail.com>
2019-04-29 16:50:25 +0300

    1 remastered

--- 1.cpp
+++ 1.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 int main() {
 	setlocale(LC_ALL, "Russian");
-	const int f = 7.481;
+	const double f = 7.481;
 	int a;
 	cout << "Введите кол-во в галлонах" << endl;
 	cin >> a;
-	cout << "Кол-во в футах\n" << a << endl;
+	cout << "Кол-во в футах\n" << a / f << endl;
 
 
 	system("pause");
