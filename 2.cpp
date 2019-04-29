c79ecc2e1538a1ce3baa23610e7945487fe8dbb8
Nikita Kurushin <nikkurushin@gmail.com>
2019-04-29 16:34:56 +0300

    2

---
+++ 2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	cout << "1990      " << "  135" << endl;
+	cout << "1991      " << " 7290" << endl;
+	cout << "1992      " << "11300" << endl;
+	cout << "1993      " << "16200" << endl;
+
+	system("pause");
+	return 0;
+}
