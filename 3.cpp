c6487408fbe5c66d977a17cbc62d712522ca58c5
Nikita Kurushin <nikkurushin@gmail.com>
2019-04-29 16:43:46 +0300

    3

---
+++ 3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	const int a = 10;
+	int b = 19 + 1;
+	int c = 20;
+	cout << a << "\n" << b << "\n" << --c << "\n";
+
+
+	system("pause");
+	return 0;
+}
